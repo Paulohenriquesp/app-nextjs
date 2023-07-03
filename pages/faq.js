caa4242334292c80120f493e522c7b4df7aafe0a
Paulo <paulosouzap7@gmail.com>
2023-07-03 14:25:20 -0300

    first commit

---
+++ pages/faq.js
@@ -0,0 +1,12 @@
+import Link from "next/link"
+
+export default function FAQPage() {
+    return (
+        <div>
+        <h1>Alura Cases - Página de Perguntas FAQ</h1>
+        <Link  href="/">
+        Ir para Home
+        </Link>
+    </div>
+    )
+}
