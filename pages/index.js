ef68f6f904f3e36d68d181042ab409a993f44cc1
Paulo <paulosouzap7@gmail.com>
2023-07-04 17:08:35 -0300

    configuracoes styles projeto

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,41 @@
+import React from 'react'
 import Link from '../src/components/Link'
+
+function GlobalStyle() {
+    return (
+        <style global jsx>
+            {`            
+            body {
+                font-family: sans-serif;
+            }
+            `}
+        </style>
+    )
+}
+
+function Title({ children, as }) {
+    const Tag = as;
+    return ( 
+        <React.Fragment>
+        <h2>
+            {children}
+        </h2>
+        <style jsx>{`
+            ${Tag} {
+                color: red;
+                font-family: sans-serif;
+            }
+            `}
+        </style>
+        </React.Fragment>
+    )
+}
 
 export default function HomePage() {
     return (
         <div>
-            <h1>Alura Cases - Home</h1>
+            <GlobalStyle />
+            <Title as="h2">Alura Cases - Home</Title>
             <Link href="/faq">
             Ir para FAQ
             </Link>
