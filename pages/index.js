28a83e9ea233fea7a8d7e712394c52b45f307f80
Paulo <paulosouzap7@gmail.com>
2023-07-05 21:40:42 -0300

    crianção screens + ajustes globalstyle

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,9 @@
-import React from "react";
+import HomeScreen from '../src/screens/HomeScreen';
+
+export default HomeScreen;
+
+
+/* import React from "react";
 import Link from "../src/components/Link";
 import Footer from "../src/components/patterns/Footer"
 
@@ -28,3 +33,4 @@ export default function HomePage() {
     </div>
   );
 }
+ */
